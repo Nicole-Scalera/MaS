5fc3119a33c47038d0b7f764f54829c74d8a6003
amazurr <alyssa.mazur1@marist.edu>
2024-10-23 22:20:56 -0400

    Basic Implementation of Character Dialogue/Interaction
    For our project's low-fidelity prototype!

--- 00 Unity Proj/Assets/Scripts/Game/Player/PlayerMovement.cs
+++ 00 Unity Proj/Assets/Scripts/Game/Player/PlayerMovement.cs
@@ -11,55 +11,70 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 _movementInput;
     private Vector2 _smoothedMovementInput;
     private Vector2 _movementInputSmoothVelocity;
-//     bool facingLeft = true;
+    private bool facingLeft = true;
 
-     //get rigid body from unity
-     private void Awake()
-     { 
-          _rigidbody = GetComponent<Rigidbody2D>();
-     }
-     //movement & smoothed movement
-     private void FixedUpdate()
-     {
-    
-     _smoothedMovementInput = Vector2.SmoothDamp(
-          _smoothedMovementInput,
-          _movementInput,
-          ref _movementInputSmoothVelocity,
-          0.08f);
-     
-     _rigidbody.velocity = _smoothedMovementInput * _speed;
-     
-          // //determine character direction
-          // if (_movementInput != 0)
-          // {
-          //      _rigidbody.AddForce(new Vector2(inputHorizontal * _speed, 0f))
-          // }
-          // if (_movementInput > 0)
-          // {
-          //      Flip();
-          // }
-          // if (_movementInput < 0)
-          // {
-          //      Flip();
-          // }
-        
-     }
-     //Get player input value from Unity
-     private void OnMove(InputValue inputValue)
-     {
+    //get rigid body from unity
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Movement & smoothed movement
+    private void FixedUpdate()
+    {
+
+        _smoothedMovementInput = Vector2.SmoothDamp(
+             _smoothedMovementInput,
+             _movementInput,
+             ref _movementInputSmoothVelocity,
+             0.08f);
+
+        _rigidbody.velocity = _smoothedMovementInput * _speed;
+
+        // //determine character direction
+        // if (_movementInput != 0)
+        // {
+        //      _rigidbody.AddForce(new Vector2(inputHorizontal * _speed, 0f))
+        // }
+        // if (_movementInput > 0)
+        // {
+        //      Flip();
+        // }
+        // if (_movementInput < 0)
+        // {
+        //      Flip();
+        // }
+
+    }
+    // Get player input value from Unity
+    private void OnMove(InputValue inputValue)
+    {
         _movementInput = inputValue.Get<Vector2>();
 
-     }
-     
-     // void Flip()
-     // {
-     //      Vector3 currentScale = gameObject.transform.localScale;
-     //      currentScale.x *= -1;
-     //      gameObject.transform.localScale = currentScale
+    }
 
-     //      facingLeft = !facingLeft;
-     // }
+    private void Update()
+    {
+        if (Keyboard.current.dKey.wasPressedThisFrame)
+        {
+            Flip();
+        }
 
+        if (Keyboard.current.aKey.wasPressedThisFrame)
+        {
+            if (!facingLeft)
+            {
+                Flip();
+            }
+        }
+    }
 
+    void Flip()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        transform.localScale = currentScale;
+
+        facingLeft = !facingLeft;
+    }
 }
